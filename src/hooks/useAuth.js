27989d250bf4bd35e4578765f8850fc36c93f9c4
Bishan Meghani <bishan.meghani@yahoo.com>
2024-04-11 00:32:28 +0100

    ready to deploy frontend

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,26 @@
+import { useSelector } from 'react-redux'
+import { selectCurrentToken } from "../features/auth/authSlice"
+import * as jwt_decode from 'jwt-decode';
+
+const useAuth = () => {
+    const token = useSelector(selectCurrentToken)
+    let isManager = false
+    let isAdmin = false
+    let status = "Employee"
+
+    if (token) {
+        const decoded = jwt_decode.jwtDecode(token)
+        const { username, roles } = decoded.UserInfo
+
+        isManager = roles.includes('Manager')
+        isAdmin = roles.includes('Admin')
+
+        if (isManager) status = "Manager"
+        if (isAdmin) status = "Admin"
+
+        return { username, roles, status, isManager, isAdmin }
+    }
+
+    return { username: '', roles: [], isManager, isAdmin, status }
+}
+export default useAuth
